90c0064b83211f91333b74e6c852c9979f1e03c8
CodinSamurai <96775952+CodinSamurai@users.noreply.github.com>
2022-06-06 13:00:13 +0100

    Update 10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    check_div = []
+    check_divisibility = []
 
     for i in range(len(my_list)):
-        if my_list[i] % 2 == 0:
-            check_div.append(True)
+        if my_list[i] % 2 != 0:
+            check_divisibility.append(False)
         else:
-            check_div.append(False)
+            check_divisibility.append(True)
 
-    return (check_div)
+    return (check_divisibility)
