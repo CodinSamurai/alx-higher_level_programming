21477a24d9729878fcb8800389ff5e98342c91e5
CodinSamurai <96775952+CodinSamurai@users.noreply.github.com>
2022-09-05 15:06:48 +0100

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,2 +1,5 @@
-const myVar = "JavaScript is amazing";
-console.log(myVar);
+#!/usr/bin/node
+const myVar = "C if fun";
+const myVar2 = "Python is cool"
+const myVar = "JavaScript is amazing"
+console.log(myVar, myVar2, myVar3);
